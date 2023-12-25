b99871e46350d7e681321e7042c3dff76731cdf8
EdenDrori <139678405+EdenDrori@users.noreply.github.com>
2023-12-25 16:32:10 +0200

    Add files via upload

--- src/joi/card.joi.ts
+++ src/joi/card.joi.ts
@@ -1,13 +1,13 @@
 import Joi from "joi";
-import { IAddress, IImage, IName, IUser } from "../@types/user";
-import { passwordRegex, phoneRegex } from "./patterns";
+import { IAddress, IImage } from "../@types/user";
+import { phoneRegex } from "./patterns";
 import { ICard } from "../@types/card";
 
 const schema = Joi.object<ICard>({
-title:Joi.string().min(1).max(50).required(),
-subtitle:Joi.string().min(1).max(50).required(),
-description:Joi.string().min(1).max(200).required(),
-web:Joi.string().min(1).max(50).allow(""),
+  title: Joi.string().min(1).max(50).required(),
+  subtitle: Joi.string().min(1).max(50).required(),
+  description: Joi.string().min(1).max(200).required(),
+  web: Joi.string().min(1).max(50).allow(""),
   address: Joi.object<IAddress>({
     state: Joi.string().min(2).max(50).allow(""),
     country: Joi.string().min(2).max(50).required(),
@@ -24,8 +24,7 @@ web:Joi.string().min(1).max(50).allow(""),
     .email({ tlds: { allow: false } })
     .required()
     .min(5),
-  phone: Joi.string().min(9).max(15).required().pattern(phoneRegex),  
-
+  phone: Joi.string().min(9).max(15).required().pattern(phoneRegex),
 });
 
 export { schema as joiCardSchema };
--- src/joi/validation.ts
+++ src/joi/validation.ts
@@ -5,15 +5,7 @@ const validation = (schema: Joi.ObjectSchema, userBody: any) => {
   if (!error) {
     return null;
   }
-//בגלל שעוצרים אחרי שגיאה אחת לא צריך את הלולאה אבל אם נרצה לעבור את כל הפרמטרים ולבדוק אותם נפעיל את :הלולאה
-  //   const { details } = error;
-  //     let errorObj: Record<string,any> = {};
 
-  //   for (let detail of details) {
-  //     let key = detail.path[0];
-  //     let { message } = detail;
-  //     errorObj[key] = message;
-  //   }
   const { message, path } = error.details[0];
   return { message, path };
 };
